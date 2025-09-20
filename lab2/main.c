446fd4769aec1a60a0c9fe060e585d10a81da492
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-09-20 23:33:43 +0300

    Update main.c

--- lab2/main.c
+++ lab2/main.c
@@ -47,3 +47,101 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+
+void print_long_format(const char *path, const char *name) {
+    struct stat st;
+    char full_path[1024];
+    snprintf(full_path, sizeof(full_path), "%s/%s", path, name);
+
+    if (lstat(full_path, &st) == -1) {
+        perror("lstat");
+        return;
+    }
+
+    // File type and permissions
+    printf((S_ISDIR(st.st_mode)) ? "d" : (S_ISLNK(st.st_mode) ? "l" : "-"));
+    printf((st.st_mode & S_IRUSR) ? "r" : "-");
+    printf((st.st_mode & S_IWUSR) ? "w" : "-");
+    printf((st.st_mode & S_IXUSR) ? "x" : "-");
+    printf((st.st_mode & S_IRGRP) ? "r" : "-");
+    printf((st.st_mode & S_IWGRP) ? "w" : "-");
+    printf((st.st_mode & S_IXGRP) ? "x" : "-");
+    printf((st.st_mode & S_IROTH) ? "r" : "-");
+    printf((st.st_mode & S_IWOTH) ? "w" : "-");
+    printf((st.st_mode & S_IXOTH) ? "x" : "-");
+
+    printf(" %2lu", (unsigned long)st.st_nlink);
+
+    struct passwd *pw = getpwuid(st.st_uid);
+    struct group *gr = getgrgid(st.st_gid);
+    if (pw != NULL) {
+        printf(" %s", pw->pw_name);
+    }
+    if (gr != NULL) {
+        printf(" %s", gr->gr_name);
+    }
+
+    printf(" %8lld", (long long)st.st_size);
+
+    char time_buf[100];
+    strftime(time_buf, sizeof(time_buf), "%b %d %H:%M", localtime(&st.st_mtime));
+    printf(" %s ", time_buf);
+}
+
+void process_path(const char *path, int show_all, int long_format, int multiple_paths) {
+    struct stat st;
+
+    if (lstat(path, &st) == -1) {
+        fprintf(stderr, "myls: cannot access '%s': No such file or directory\n", path);
+        return;
+    }
+
+    if (S_ISDIR(st.st_mode)) {
+        if (multiple_paths) {
+            printf("%s:\n", path);
+        }
+        DIR *d = opendir(path);
+        if (d == NULL) {
+            perror("opendir");
+            return;
+        }
+
+        struct dirent **namelist;
+        int n = scandir(path, &namelist, NULL, alphasort);
+        if (n < 0) {
+            perror("scandir");
+            closedir(d);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!show_all && namelist[i]->d_name[0] == '.') {
+                free(namelist[i]);
+                continue;
+            }
+
+            if (long_format) {
+                print_long_format(path, namelist[i]->d_name);
+                printf("%s\n", namelist[i]->d_name);
+            } else {
+                printf("%-16s", namelist[i]->d_name);
+            }
+            free(namelist[i]);
+        }
+        if (!long_format) {
+            printf("\n");
+        }
+        free(namelist);
+        closedir(d);
+    } else {
+        if (long_format) {
+            char *dirc = strdup(path);
+            char *basec = strdup(path);
+            print_long_format(dirname(dirc), basename(basec));
+            free(dirc);
+            free(basec);
+        }
+        printf("%s\n", path);
+    }
+}
