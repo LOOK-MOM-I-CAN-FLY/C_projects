e5e7cc89e6014761543671c5160ced19fe49d36d
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-09-20 14:23:35 +0300

    Update main.c

--- lab2/main.c
+++ lab2/main.c
@@ -12,3 +12,38 @@
 
 void print_long_format(const char *path, const char *name);
 void process_path(const char *path, int show_all, int long_format, int multiple_paths);
+
+int main(int argc, char *argv[]) {
+    int opt;
+    int show_all = 0;
+    int long_format = 0;
+
+    while ((opt = getopt(argc, argv, "la")) != -1) {
+        switch (opt) {
+            case 'l':
+                long_format = 1;
+                break;
+            case 'a':
+                show_all = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-la] [file...]\n", argv[0]);
+                exit(1);
+        }
+    }
+
+    int multiple_paths = (argc - optind) > 1;
+
+    if (optind == argc) {
+        process_path(".", show_all, long_format, 0);
+    } else {
+        for (int i = optind; i < argc; i++) {
+            process_path(argv[i], show_all, long_format, multiple_paths);
+            if (multiple_paths && i < argc - 1) {
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
