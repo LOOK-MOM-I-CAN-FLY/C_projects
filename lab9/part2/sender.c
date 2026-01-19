e9dc47dd767bc7352c0de0fd3154de14ab2a66e7
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2026-01-19 08:56:13 +0300

    Update sender.c
    Добавлена функция handle_sigint, чтобы при завершении программы ресурсы корректно удалялись из системы.

--- lab9/part2/sender.c
+++ lab9/part2/sender.c
@@ -7,8 +7,13 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <sys/types.h>
+#include <signal.h> 
 
 #define SHM_SIZE 1024
+
+int shmid;
+int semid;
+char *shared_mem;
 
 union semun {
     int val;
@@ -16,31 +21,46 @@ union semun {
     unsigned short *array;
 };
 
-struct sembuf lock_res = {0, -1, 0}; 
-struct sembuf rel_res = {0, 1, 0};   
+struct sembuf lock_res = {0, -1, 0};
+struct sembuf rel_res = {0, 1, 0};
+
+void handle_sigint(int sig) {
+    printf("\nЗавершение работы sender...\n");
+    
+    if (shmdt(shared_mem) == -1) perror("shmdt");
+    
+    if (shmctl(shmid, IPC_RMID, NULL) == -1) perror("shmctl");
+    else printf("Shared memory removed.\n");
+
+    if (semctl(semid, 0, IPC_RMID) == -1) perror("semctl");
+    else printf("Semaphores removed.\n");
+
+    exit(0);
+}
 
 int main() {
+    signal(SIGINT, handle_sigint);
 
     key_t key = ftok(".", 'A');
     if (key == -1) { perror("ftok"); exit(1); }
 
-    int shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
+    shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
     if (shmid == -1) { perror("shmget"); exit(1); }
 
-    char *shared_mem = (char*)shmat(shmid, NULL, 0);
+    shared_mem = (char*)shmat(shmid, NULL, 0);
     if (shared_mem == (char*)(-1)) { perror("shmat"); exit(1); }
 
-    int semid = semget(key, 1, IPC_CREAT | 0666);
+    semid = semget(key, 1, IPC_CREAT | 0666);
     if (semid == -1) { perror("semget"); exit(1); }
 
     union semun arg;
     arg.val = 1;
     if (semctl(semid, 0, SETVAL, arg) == -1) { perror("semctl"); exit(1); }
 
-    printf("\033[0;31mSender запущен (PID: %d)\033[0m. Пишу данные каждые 3 секунды...\n", getpid());
+    printf("\033[0;31mSender запущен (PID: %d)\033[0m. Нажмите Ctrl+C для выхода и очистки ресурсов.\n", getpid());
 
     while(1) {
-        if (semop(semid, &lock_res, 1) == -1) { perror("semop lock"); exit(1); }
+        if (semop(semid, &lock_res, 1) == -1) { perror("semop lock"); break; }
 
         time_t now = time(NULL);
         char *time_str = ctime(&now);
@@ -48,9 +68,10 @@ int main() {
 
         snprintf(shared_mem, SHM_SIZE, "Time: %s | Sender PID: %d", time_str, getpid());
 
-
-        if (semop(semid, &rel_res, 1) == -1) { perror("semop unlock"); exit(1); }
+        if (semop(semid, &rel_res, 1) == -1) { perror("semop unlock"); break; }
 
         sleep(3);
     }
+    
+    return 0;
 }
