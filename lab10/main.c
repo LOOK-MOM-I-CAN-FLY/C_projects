3389bedb476050a61047ef7ea965b88df69b629a
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2026-01-19 09:08:29 +0300

    Update main.c
    В этом коде удалены барьеры, добавлен корректный выход через флаг и обязательно добавлен pthread_join.

--- lab10/main.c
+++ lab10/main.c
@@ -13,24 +13,22 @@
 
 char shared_array[BUFFER_SIZE] = "Empty";
 pthread_rwlock_t rwlock;
-pthread_barrier_t barrier_ready;
-pthread_barrier_t barrier_done;
+
 int record_counter = 0;
-int keep_running = 1;
+volatile int keep_running = 1; 
 
 void* writer_thread(void* arg) {
     (void)arg;
     while (keep_running) {
         pthread_rwlock_wrlock(&rwlock);
+        
         record_counter++;
         snprintf(shared_array, BUFFER_SIZE, "Record ID: %d", record_counter);
-        printf(COLOR_RED "[WRITER] Updated array to: %s" COLOR_RESET "\n", shared_array);
+        printf(COLOR_RED "[WRITER] Обновил массив: %s" COLOR_RESET "\n", shared_array);
+        
         pthread_rwlock_unlock(&rwlock);
 
-        pthread_barrier_wait(&barrier_ready);
-        pthread_barrier_wait(&barrier_done);
-        
-        usleep(500000);
+        usleep(500000); 
     }
     return NULL;
 }
@@ -38,13 +36,15 @@ void* writer_thread(void* arg) {
 void* reader_thread(void* arg) {
     long tid = (long)arg;
     while (keep_running) {
-        pthread_barrier_wait(&barrier_ready);
-
         pthread_rwlock_rdlock(&rwlock);
-        printf(COLOR_GREEN "[READER %ld] Current state: %s" COLOR_RESET "\n", tid, shared_array);
+        
+        if (keep_running) { 
+            printf(COLOR_GREEN "[READER %ld] Прочитал: %s" COLOR_RESET "\n", tid, shared_array);
+        }
+
         pthread_rwlock_unlock(&rwlock);
 
-        pthread_barrier_wait(&barrier_done);
+        usleep(200000);
     }
     return NULL;
 }
@@ -53,26 +53,39 @@ int main() {
     pthread_t readers[NUM_READERS];
     pthread_t writer;
 
-    pthread_rwlock_init(&rwlock, NULL);
-    pthread_barrier_init(&barrier_ready, NULL, NUM_READERS + 1);
-    pthread_barrier_init(&barrier_done, NULL, NUM_READERS + 1);
+    if (pthread_rwlock_init(&rwlock, NULL) != 0) {
+        perror("rwlock_init");
+        return 1;
+    }
 
-    pthread_create(&writer, NULL, writer_thread, NULL);
+    printf("--- Запуск потоков. Нажмите ENTER для остановки ---\n");
+
+    if (pthread_create(&writer, NULL, writer_thread, NULL) != 0) {
+        perror("create writer");
+        return 1;
+    }
+
     for (long i = 0; i < NUM_READERS; i++) {
-        pthread_create(&readers[i], NULL, reader_thread, (void*)i);
+        if (pthread_create(&readers[i], NULL, reader_thread, (void*)i) != 0) {
+            perror("create reader");
+            return 1;
+        }
     }
 
     getchar();
-    keep_running = 0;
+    
+    printf("Завершение работы... Ожидание потоков...\n");
+    keep_running = 0; 
 
-    pthread_cancel(writer);
+    pthread_join(writer, NULL);
+
     for (int i = 0; i < NUM_READERS; i++) {
-        pthread_cancel(readers[i]);
+        pthread_join(readers[i], NULL);
     }
+    
+    printf("Все потоки завершены корректно.\n");
 
     pthread_rwlock_destroy(&rwlock);
-    pthread_barrier_destroy(&barrier_ready);
-    pthread_barrier_destroy(&barrier_done);
 
     return 0;
 }
